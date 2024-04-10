623c41f7799bd65ac9064eac1dd19f5e8db78f91
John Källström <johnkaellstrom@gmail.com>
2024-04-10 15:03:37 +0200

    Initial migration

--- Storagge/Persistence/Migrations/20240410125404_Initial.cs
+++ Storagge/Persistence/Migrations/20240410125404_Initial.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace Storagge.Persistence.Migrations
 {
     /// <inheritdoc />
@@ -19,7 +21,7 @@ namespace Storagge.Persistence.Migrations
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Price = table.Column<int>(type: "int", nullable: false),
-                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2024, 4, 9, 17, 39, 7, 406, DateTimeKind.Local).AddTicks(6622)),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2024, 4, 10, 14, 54, 4, 620, DateTimeKind.Local).AddTicks(7515)),
                     Category = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Shelf = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Count = table.Column<int>(type: "int", nullable: false),
@@ -28,6 +30,18 @@ namespace Storagge.Persistence.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Product", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "Category", "Count", "Description", "Name", "Price", "Shelf" },
+                values: new object[,]
+                {
+                    { 1, "Entertainment", 10, "A boardgame about fighting invading alien ships!", "Under Falling Skies", 299, "A15" },
+                    { 2, "Outdoors", 3, "A sturdy pair of hiking boots.", "Hanwag Hiking Boots", 1099, "B07" },
+                    { 3, "Kitchen", 5, "Great coffee machine.", "Moccamaster", 1499, "C19" },
+                    { 4, "Plants", 15, "An easy to care for plant that fits most homes!", "Monstera Delicsiosa", 199, "D01" },
+                    { 5, "Entertainment", 15, "An awesome videogame!", "Zelda: Breath of the Wild", 699, "A08" }
                 });
         }
 
