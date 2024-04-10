32678960c3f88574265b1262693f79f41727b125
John Källström <johnkaellstrom@gmail.com>
2024-04-10 15:37:47 +0200

    Data annotations

--- Storagge/Models/Product.cs
+++ Storagge/Models/Product.cs
@@ -3,12 +3,31 @@
     public class Product
     {
         public int Id { get; set; }
+
+        [StringLength(50)]
+        [Required]
         public string? Name { get; set; }
+
+        [Range(0, double.MaxValue)]
         public int Price { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
         public DateTime OrderDate { get; set; }
+
+        [StringLength(20)]
+        [Required]
         public string? Category { get; set; }
+
+        [StringLength(5)]
+        [Required]
         public string? Shelf { get; set; }
+
+        [Range(0, double.MaxValue)]
         public int Count { get; set; }
+
+        [StringLength(300)]
+        [Required]
         public string? Description { get; set; }
     }
 }
--- Storagge/Usings.cs
+++ Storagge/Usings.cs
@@ -4,3 +4,4 @@ global using Storagge.Persistence;
 global using Storagge.Models;
 global using Storagge.Persistence.EntityConfigurations;
 global using Microsoft.AspNetCore.Mvc;
+global using System.ComponentModel.DataAnnotations;
